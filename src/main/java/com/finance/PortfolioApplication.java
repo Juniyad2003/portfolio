2344dc1764d2c31c95ebd0d44dfa718b23fa814d
= <=>
2026-02-05 04:49:00 +0000

    Minor theme changes

--- src/main/java/com/finance/PortfolioApplication.java
+++ src/main/java/com/finance/PortfolioApplication.java
@@ -2,10 +2,8 @@ package com.finance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
 public class PortfolioApplication {
 
     public static void main(String[] args) {
